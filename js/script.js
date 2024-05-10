61ae6ed6b78797e8470ad2c33ef45ea6b2ae6fe7
admin-redes-fisei <enriquotero@gmail.com>
2024-05-09 22:03:14 -0500

    js mejorado

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 function checkAnswer(answer) {
     var messageDiv = document.getElementById('message');
-    var score = document.getElementById('score');
+    var scoreCount = document.getElementById('scoreCount');
     var correctAnswer = true; // Respuesta correcta para esta pregunta
     
     if (answer === correctAnswer) {
         messageDiv.textContent = "¡Correcto!";
         messageDiv.classList.add('correct');
         updateScore(10); // Suma 10 puntos por respuesta correcta
+        var currentScore = parseInt(scoreCount.textContent);
+        localStorage.setItem('score', currentScore); // Almacenar el puntaje en localStorage
+        setTimeout(function() {
+            window.location.href = 'verdadero_falso2.html'; // Redirigir a la siguiente pregunta
+        }, 2000); // Esperar 2 segundos antes de redirigir
     } else {
         messageDiv.textContent = "¡Incorrecto!";
         messageDiv.classList.add('incorrect');
@@ -18,7 +23,7 @@ function checkAnswer(answer) {
 }
 
 function updateScore(points) {
-    var scoreElement = document.getElementById('score');
+    var scoreElement = document.getElementById('scoreCount');
     var currentScore = parseInt(scoreElement.textContent);
     scoreElement.textContent = currentScore + points;
 }
