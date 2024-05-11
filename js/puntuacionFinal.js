34dccbaae86f155258710071775b46f055827a93
Anthony Solis <asolis7378@uta.edu.ec>
2024-05-11 01:31:44 -0500

    COREECCION CONTADOR PUNTOS

--- js/puntuacionFinal.js
+++ js/puntuacionFinal.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Obtener las puntuaciones guardadas en localStorage
-    var puntuacionA = localStorage.getItem('groupAPoints');
-    var puntuacionB = localStorage.getItem('groupBPoints');
+    // Obtener las puntuaciones guardadas en localStorage para el grupo A
+    var puntuacionAMultipleChoice = parseInt(localStorage.getItem('groupAPoints') || '0');
+    var puntuacionAVerdaderoFalso = parseInt(localStorage.getItem('groupAPointsVF') || '0');
 
-    // Actualizar los campos de puntuación en el HTML
-    document.getElementById('puntuacion-a').value = puntuacionA || '0';
-    document.getElementById('puntuacion-b').value = puntuacionB || '0';
+    // Obtener las puntuaciones guardadas en localStorage para el grupo B
+    var puntuacionBMultipleChoice = parseInt(localStorage.getItem('groupBPoints') || '0');
+    var puntuacionBVerdaderoFalso = parseInt(localStorage.getItem('groupBPointsVF') || '0');
+
+    // Calcular la puntuación total del grupo A sumando las puntuaciones de ambas modalidades
+    var puntuacionATotal = puntuacionAMultipleChoice + puntuacionAVerdaderoFalso;
+
+    // Calcular la puntuación total del grupo B sumando las puntuaciones de ambas modalidades
+    var puntuacionBTotal = puntuacionBMultipleChoice + puntuacionBVerdaderoFalso;
+
+    // Actualizar el campo de puntuación en el HTML para el grupo A
+    document.getElementById('puntuacion-a').value = puntuacionATotal.toString();
+
+    // Actualizar el campo de puntuación en el HTML para el grupo B
+    document.getElementById('puntuacion-b').value = puntuacionBTotal.toString();
 });
