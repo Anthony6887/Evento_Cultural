fbf17faca3463e0d809c109d41e23899baa76855
Anthony Solis <asolis7378@uta.edu.ec>
2024-05-11 23:40:38 -0500

    Demo Final

--- js/puntuacionFinal.js
+++ js/puntuacionFinal.js
@@ -2,16 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
     // Obtener las puntuaciones guardadas en localStorage para el grupo A
     var puntuacionAMultipleChoice = parseInt(localStorage.getItem('groupAPoints') || '0');
     var puntuacionAVerdaderoFalso = parseInt(localStorage.getItem('groupAPointsVF') || '0');
+    var puntuacionAPista = parseInt(localStorage.getItem('groupAPointsPista') || '0');
 
     // Obtener las puntuaciones guardadas en localStorage para el grupo B
     var puntuacionBMultipleChoice = parseInt(localStorage.getItem('groupBPoints') || '0');
     var puntuacionBVerdaderoFalso = parseInt(localStorage.getItem('groupBPointsVF') || '0');
+    var puntuacionBPista = parseInt(localStorage.getItem('groupBPointsPista') || '0');
 
     // Calcular la puntuación total del grupo A sumando las puntuaciones de ambas modalidades
-    var puntuacionATotal = puntuacionAMultipleChoice + puntuacionAVerdaderoFalso;
+    var puntuacionATotal = puntuacionAMultipleChoice + puntuacionAVerdaderoFalso + puntuacionAPista;
 
     // Calcular la puntuación total del grupo B sumando las puntuaciones de ambas modalidades
-    var puntuacionBTotal = puntuacionBMultipleChoice + puntuacionBVerdaderoFalso;
+    var puntuacionBTotal = puntuacionBMultipleChoice + puntuacionBVerdaderoFalso + puntuacionBPista;
+
 
     // Actualizar el campo de puntuación en el HTML para el grupo A
     document.getElementById('puntuacion-a').value = puntuacionATotal.toString();
@@ -26,6 +29,8 @@ function reiniciarPuntuaciones() {
     localStorage.removeItem('groupAPointsVF');
     localStorage.removeItem('groupBPoints');
     localStorage.removeItem('groupBPointsVF');
+    localStorage.removeItem('groupAPointsPista');
+    localStorage.removeItem('groupBPointsPista');
 
     localStorage.removeItem('respuesta_pregunta1');
     localStorage.removeItem('respuesta_pregunta2');
@@ -40,6 +45,16 @@ function reiniciarPuntuaciones() {
     localStorage.removeItem('respuesta_pregunta4VF');
     localStorage.removeItem('respuesta_pregunta5VF');
     localStorage.removeItem('respuesta_pregunta6VF');
+
+    localStorage.removeItem('respuesta_pregunta1Pista');
+    localStorage.removeItem('respuesta_pregunta2Pista');
+    localStorage.removeItem('respuesta_pregunta3Pista');
+    localStorage.removeItem('respuesta_pregunta4Pista');
+
+
+    localStorage.removeItem('respuesta_pregunta1PistaI');
+
+    localStorage.removeItem('respuesta_vacia_pregunta1');
 
     localStorage.removeItem('respuesta_incorrecta_pregunta1');
     localStorage.removeItem('respuesta_incorrecta_pregunta2');
@@ -61,16 +76,22 @@ function reiniciarPuntuaciones() {
     document.getElementById('puntuacion-b').value = '0';
 }
 function mostrarGanador() {
+
+    // Obtener los puntos del grupo A y del grupo B del localStorage
     var puntuacionA = parseInt(localStorage.getItem('groupAPoints') || '0');
     var puntuacionB = parseInt(localStorage.getItem('groupBPoints') || '0');
-    
+
+    var sumaPuntosGrupoA = puntuacionA + parseInt(localStorage.getItem('groupAPointsVF') || '0') + parseInt(localStorage.getItem('groupAPointsPista') || '0');
+    var sumaPuntosGrupoB = puntuacionB + parseInt(localStorage.getItem('groupBPointsVF') || '0') + parseInt(localStorage.getItem('groupBPointsPista') || '0');
+
+
     var mensaje;
-    if (puntuacionA > puntuacionB) {
-        mensaje = 'El ganador es el Grupo A';
-    } else if (puntuacionB > puntuacionA) {
-        mensaje = 'El ganador es el Grupo B';
+    if (sumaPuntosGrupoA > sumaPuntosGrupoB) {
+        mensaje = 'El ganador es el Grupo A con ' + sumaPuntosGrupoA + ' puntos.';
+    } else if (sumaPuntosGrupoA < sumaPuntosGrupoB) {
+        mensaje = 'El ganador es el Grupo B con ' + sumaPuntosGrupoB + ' puntos.';
     } else {
-        mensaje = '¡Empate!';
+        mensaje = '¡Empate! Ambos grupos tienen ' + sumaPuntosGrupoA + ' puntos.';
     }
 
     // Crear la URL con el mensaje como parámetro
